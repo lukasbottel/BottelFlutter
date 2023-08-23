45eeb9a6cfd5a044986ec125a7ba335c11af9adc
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-23 15:38:26 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_model.dart
+++ lib/pages/home_page/home_page_model.dart
@@ -1,3 +1,4 @@
+import '/flutter_flow/flutter_flow_google_map.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/pages/nav_bar1/nav_bar1_widget.dart';
@@ -9,6 +10,9 @@ class HomePageModel extends FlutterFlowModel {
   ///  State fields for stateful widgets in this page.
 
   final unfocusNode = FocusNode();
+  // State field(s) for GoogleMap widget.
+  LatLng? googleMapsCenter;
+  final googleMapsController = Completer<GoogleMapController>();
   // Model for NavBar1 component.
   late NavBar1Model navBar1Model;
 
--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -1,3 +1,4 @@
+import '/flutter_flow/flutter_flow_google_map.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/pages/nav_bar1/nav_bar1_widget.dart';
@@ -40,9 +41,10 @@ class _HomePageWidgetState extends State<HomePageWidget> {
       onTap: () => FocusScope.of(context).requestFocus(_model.unfocusNode),
       child: Scaffold(
         key: scaffoldKey,
+        resizeToAvoidBottomInset: false,
         backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
         appBar: AppBar(
-          backgroundColor: Color(0xFFE90000),
+          backgroundColor: Color(0x46E90000),
           iconTheme: IconThemeData(color: Color(0xFFEA7B7B)),
           automaticallyImplyLeading: false,
           actions: [
@@ -104,10 +106,32 @@ class _HomePageWidgetState extends State<HomePageWidget> {
         ),
         body: SafeArea(
           top: true,
-          child: wrapWithModel(
-            model: _model.navBar1Model,
-            updateCallback: () => setState(() {}),
-            child: NavBar1Widget(),
+          child: Stack(
+            children: [
+              FlutterFlowGoogleMap(
+                controller: _model.googleMapsController,
+                onCameraIdle: (latLng) => _model.googleMapsCenter = latLng,
+                initialLocation: _model.googleMapsCenter ??=
+                    LatLng(13.106061, -59.613158),
+                markerColor: GoogleMarkerColor.violet,
+                mapType: MapType.normal,
+                style: GoogleMapStyle.standard,
+                initialZoom: 14.0,
+                allowInteraction: true,
+                allowZoom: true,
+                showZoomControls: true,
+                showLocation: true,
+                showCompass: false,
+                showMapToolbar: false,
+                showTraffic: false,
+                centerMapOnMarkerTap: true,
+              ),
+              wrapWithModel(
+                model: _model.navBar1Model,
+                updateCallback: () => setState(() {}),
+                child: NavBar1Widget(),
+              ),
+            ],
           ),
         ),
       ),
