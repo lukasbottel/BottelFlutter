2c541ce0668f59ed6abdcfba38fc9c48b75acb2a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-23 16:52:25 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/nav_bar1/nav_bar1_widget.dart
+++ lib/pages/nav_bar1/nav_bar1_widget.dart
@@ -43,228 +43,240 @@ class _NavBar1WidgetState extends State<NavBar1Widget> {
 
     return Align(
       alignment: AlignmentDirectional(0.0, 1.0),
-      child: Container(
-        width: double.infinity,
-        height: 95.0,
-        decoration: BoxDecoration(
-          color: Color(0x00EEEEEE),
-        ),
-        child: Stack(
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Material(
-                  color: Colors.transparent,
-                  elevation: 0.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0.0),
-                  ),
-                  child: Container(
-                    width: double.infinity,
-                    height: 75.0,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [Colors.white, Color(0xFFFEFEFE)],
-                        stops: [0.0, 1.0],
-                        begin: AlignmentDirectional(0.0, -1.0),
-                        end: AlignmentDirectional(0, 1.0),
-                      ),
+      child: Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 30.0),
+        child: Container(
+          width: double.infinity,
+          height: 95.0,
+          decoration: BoxDecoration(
+            color: Color(0x00EEEEEE),
+          ),
+          child: Stack(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Material(
+                    color: Colors.transparent,
+                    elevation: 0.0,
+                    shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(0.0),
                     ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(10.0, 10.0, 0.0, 0.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FlutterFlowIconButton(
-                        borderColor: Colors.transparent,
-                        borderRadius: 30.0,
-                        borderWidth: 1.0,
-                        buttonSize: 40.0,
-                        icon: Icon(
-                          FFIcons.kwallethome,
-                          color: Color(0xFFBDBDBD),
-                          size: 24.0,
+                    child: Container(
+                      width: double.infinity,
+                      height: 75.0,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Colors.white, Color(0xFFFEFEFE)],
+                          stops: [0.0, 1.0],
+                          begin: AlignmentDirectional(0.0, -1.0),
+                          end: AlignmentDirectional(0, 1.0),
                         ),
-                        onPressed: () async {
-                          context.pushNamed('Wallet');
-                        },
+                        borderRadius: BorderRadius.circular(0.0),
                       ),
-                      Text(
-                        'Wallet',
-                        style: FlutterFlowTheme.of(context).bodyMedium.override(
-                              fontFamily: 'Readex Pro',
-                              color: Color(0xDFAEAEAE),
-                              fontSize: 8.5,
-                            ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 0.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FlutterFlowIconButton(
-                        borderColor: Colors.transparent,
-                        borderRadius: 30.0,
-                        borderWidth: 1.0,
-                        buttonSize: 40.0,
-                        icon: Icon(
-                          FFIcons.kmaphome,
-                          color: Color(0xFFBDBDBD),
-                          size: 24.0,
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding:
+                        EdgeInsetsDirectional.fromSTEB(10.0, 10.0, 0.0, 0.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FlutterFlowIconButton(
+                          borderColor: Colors.transparent,
+                          borderRadius: 30.0,
+                          borderWidth: 1.0,
+                          buttonSize: 40.0,
+                          icon: Icon(
+                            FFIcons.kwallethome,
+                            color: Color(0xFFBDBDBD),
+                            size: 24.0,
+                          ),
+                          onPressed: () async {
+                            context.pushNamed('Wallet');
+                          },
                         ),
-                        onPressed: () async {
-                          context.pushNamed('HomePage');
-                        },
-                      ),
-                      Text(
-                        'Map',
-                        style: FlutterFlowTheme.of(context).bodyMedium.override(
-                              fontFamily: 'Readex Pro',
-                              color: Color(0xDFAEAEAE),
-                              fontSize: 8.5,
-                            ),
-                      ),
-                    ],
+                        Text(
+                          'Wallet',
+                          style:
+                              FlutterFlowTheme.of(context).bodyMedium.override(
+                                    fontFamily: 'Readex Pro',
+                                    color: Color(0xDFAEAEAE),
+                                    fontSize: 8.5,
+                                  ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(5.0, 0.0, 5.0, 5.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 75.0,
-                        height: 75.0,
-                        child: Stack(
-                          children: [
-                            Material(
-                              color: Colors.transparent,
-                              elevation: 2.0,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(35.0),
+                  Padding(
+                    padding:
+                        EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 0.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FlutterFlowIconButton(
+                          borderColor: Colors.transparent,
+                          borderRadius: 30.0,
+                          borderWidth: 1.0,
+                          buttonSize: 40.0,
+                          icon: Icon(
+                            FFIcons.kmaphome,
+                            color: Color(0xFFBDBDBD),
+                            size: 24.0,
+                          ),
+                          onPressed: () async {
+                            context.pushNamed('HomePage');
+                          },
+                        ),
+                        Text(
+                          'Map',
+                          style:
+                              FlutterFlowTheme.of(context).bodyMedium.override(
+                                    fontFamily: 'Readex Pro',
+                                    color: Color(0xDFAEAEAE),
+                                    fontSize: 8.5,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsetsDirectional.fromSTEB(5.0, 0.0, 5.0, 20.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 75.0,
+                          height: 75.0,
+                          child: Stack(
+                            children: [
+                              Material(
+                                color: Colors.transparent,
+                                elevation: 2.0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(35.0),
+                                ),
+                                child: Container(
+                                  width: 75.0,
+                                  height: 75.0,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFF54DC9E),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        blurRadius: 12.0,
+                                        color: Color(0x2B30D28B),
+                                        offset: Offset(0.0, 2.0),
+                                        spreadRadius: 5.0,
+                                      )
+                                    ],
+                                    borderRadius: BorderRadius.circular(35.0),
+                                  ),
+                                ),
                               ),
-                              child: Container(
+                              Container(
                                 width: 75.0,
                                 height: 75.0,
                                 decoration: BoxDecoration(
-                                  color: Color(0xFF54DC9E),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      blurRadius: 12.0,
-                                      color: Color(0x2B30D28B),
-                                      offset: Offset(0.0, 2.0),
-                                      spreadRadius: 5.0,
-                                    )
-                                  ],
                                   borderRadius: BorderRadius.circular(35.0),
                                 ),
-                              ),
-                            ),
-                            Container(
-                              width: 75.0,
-                              height: 75.0,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(35.0),
-                              ),
-                              child: Padding(
-                                padding: EdgeInsetsDirectional.fromSTEB(
-                                    0.0, 3.5, 0.0, 0.0),
-                                child: Icon(
-                                  FFIcons.kscanhome2,
-                                  color: Color(0xFFFEFEFE),
-                                  size: 27.0,
+                                child: Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0.0, 3.5, 0.0, 0.0),
+                                  child: Icon(
+                                    FFIcons.kscanhome2,
+                                    color: Color(0xFFFEFEFE),
+                                    size: 27.0,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 0.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FlutterFlowIconButton(
-                        borderColor: Colors.transparent,
-                        borderRadius: 30.0,
-                        borderWidth: 1.0,
-                        buttonSize: 40.0,
-                        icon: FaIcon(
-                          FontAwesomeIcons.store,
-                          color: Color(0xFFC7C7C7),
-                          size: 24.0,
+                  Padding(
+                    padding:
+                        EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 0.0, 0.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FlutterFlowIconButton(
+                          borderColor: Colors.transparent,
+                          borderRadius: 30.0,
+                          borderWidth: 1.0,
+                          buttonSize: 40.0,
+                          icon: FaIcon(
+                            FontAwesomeIcons.store,
+                            color: Color(0xFFC7C7C7),
+                            size: 24.0,
+                          ),
+                          onPressed: () async {
+                            context.pushNamed('StoreCopyCopyCopy');
+                          },
                         ),
-                        onPressed: () async {
-                          context.pushNamed('StoreCopyCopyCopy');
-                        },
-                      ),
-                      Text(
-                        'Store',
-                        style: FlutterFlowTheme.of(context).bodyMedium.override(
-                              fontFamily: 'Readex Pro',
-                              color: Color(0xDFAEAEAE),
-                              fontSize: 8.5,
-                            ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 10.0, 0.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FlutterFlowIconButton(
-                        borderColor: Colors.transparent,
-                        borderRadius: 30.0,
-                        borderWidth: 1.0,
-                        buttonSize: 40.0,
-                        icon: Icon(
-                          FFIcons.ksettingshome,
-                          color: Color(0xFFC7C7C7),
-                          size: 24.0,
+                        Text(
+                          'Store',
+                          style:
+                              FlutterFlowTheme.of(context).bodyMedium.override(
+                                    fontFamily: 'Readex Pro',
+                                    color: Color(0xDFAEAEAE),
+                                    fontSize: 8.5,
+                                  ),
                         ),
-                        onPressed: () async {
-                          context.pushNamed('Settings');
-                        },
-                      ),
-                      Text(
-                        'Settings',
-                        style: FlutterFlowTheme.of(context).bodyMedium.override(
-                              fontFamily: 'Readex Pro',
-                              color: Color(0xDFAEAEAE),
-                              fontSize: 8.5,
-                            ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Padding(
+                    padding:
+                        EdgeInsetsDirectional.fromSTEB(0.0, 10.0, 10.0, 0.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FlutterFlowIconButton(
+                          borderColor: Colors.transparent,
+                          borderRadius: 30.0,
+                          borderWidth: 1.0,
+                          buttonSize: 40.0,
+                          icon: Icon(
+                            FFIcons.ksettingshome,
+                            color: Color(0xFFC7C7C7),
+                            size: 24.0,
+                          ),
+                          onPressed: () async {
+                            context.pushNamed('Settings');
+                          },
+                        ),
+                        Text(
+                          'Settings',
+                          style:
+                              FlutterFlowTheme.of(context).bodyMedium.override(
+                                    fontFamily: 'Readex Pro',
+                                    color: Color(0xDFAEAEAE),
+                                    fontSize: 8.5,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
