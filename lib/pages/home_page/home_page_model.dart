cf708849d4ca5df9de1bd0abf40f9a8439b7786c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-23 21:55:20 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_model.dart
+++ lib/pages/home_page/home_page_model.dart
@@ -2,7 +2,6 @@ import '/flutter_flow/flutter_flow_google_map.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/pages/nav_bar1/nav_bar1_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -14,18 +13,13 @@ class HomePageModel extends FlutterFlowModel {
   // State field(s) for GoogleMap widget.
   LatLng? googleMapsCenter;
   final googleMapsController = Completer<GoogleMapController>();
-  // Model for NavBar1 component.
-  late NavBar1Model navBar1Model;
 
   /// Initialization and disposal methods.
 
-  void initState(BuildContext context) {
-    navBar1Model = createModel(context, () => NavBar1Model());
-  }
+  void initState(BuildContext context) {}
 
   void dispose() {
     unfocusNode.dispose();
-    navBar1Model.dispose();
   }
 
   /// Action blocks are added here.
--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -2,7 +2,6 @@ import '/flutter_flow/flutter_flow_google_map.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/pages/nav_bar1/nav_bar1_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -63,11 +62,6 @@ class _HomePageWidgetState extends State<HomePageWidget> {
               showMapToolbar: false,
               showTraffic: false,
               centerMapOnMarkerTap: false,
-            ),
-            wrapWithModel(
-              model: _model.navBar1Model,
-              updateCallback: () => setState(() {}),
-              child: NavBar1Widget(),
             ),
           ],
         ),
