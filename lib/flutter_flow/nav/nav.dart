56bbbcde0fa338c7a37770fab1c05a521fc757ca
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-23 22:04:21 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -76,11 +76,6 @@ GoRouter createRouter(AppStateNotifier appStateNotifier) => GoRouter(
           name: 'StoreCopyCopyCopy',
           path: '/storeCopyCopyCopy',
           builder: (context, params) => StoreCopyCopyCopyWidget(),
-        ),
-        FFRoute(
-          name: 'HomePageCopy',
-          path: '/homePageCopy',
-          builder: (context, params) => HomePageCopyWidget(),
         )
       ].map((r) => r.toRoute(appStateNotifier)).toList(),
     );
